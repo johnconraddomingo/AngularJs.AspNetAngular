a890f247bc1e182805c8ff3d3b0e7c20252ae984
John Conrad Domingo <john.conrad.domingo@outlook.com>
2017-07-20 23:46:45 +1000

    Added WebApi. Search + Reset

---
+++ AngularJs.AspNetAngular/Controllers/PatientsController.cs
@@ -0,0 +1,57 @@
+﻿using AngularJs.AspNetAngular.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using System.Web.Http;
+
+namespace AngularJs.AspNetAngular.Controllers
+{
+
+    public class PatientsController : ApiController
+    {
+        List<Patient> patients = new List<Patient>
+       {
+            new Patient { Id = 1, FirstName = "Oliver", LastName = "Queen", Age = 1 },
+            new Patient { Id = 2, FirstName = "Damian", LastName = "Wayne", Age = 3 },
+            new Patient { Id = 3, FirstName = "Diana", LastName = "Prince", Age = 5 }
+       };
+
+        [HttpGet]
+        [Route("Patients")]
+        public IHttpActionResult GetPatients()
+        {
+            return Ok(patients);
+        }
+
+        [HttpGet]
+        [Route("Patient/{id}")]
+        public IHttpActionResult GetPatient(int id)
+        {
+            var patientHere = patients.FirstOrDefault((p) => p.Id == id);
+            if (patientHere == null)
+            {
+                return NotFound();
+            }
+            return Ok(patientHere);
+        }
+
+
+        [HttpGet]
+        [Route("Patient/Search/{searchTerm}")]
+        public IHttpActionResult SearchPatients(string searchTerm)
+        {
+            if (searchTerm == "undefined" || searchTerm == "")
+                return GetPatients();
+
+            var patientsFound = patients.Where(s => s.FirstName.Contains(searchTerm) || s.LastName.Contains(searchTerm)); 
+            if (patientsFound == null)
+            {
+                return NotFound();
+            }
+            return Ok(patientsFound);
+        }
+
+    }
+}
--- AngularJs.AspNetAngular/Scripts/PatientManagement/app.js
+++ AngularJs.AspNetAngular/Scripts/PatientManagement/app.js
@@ -4,15 +4,59 @@
     var PatientController = function ($scope, $http) {
 
         $scope.Header = "Patients";
-        $scope.Patients = [
-            {
-                FirstName: "John",
-                LastName: "Conrad"
-            },
-            {
-                FirstName: "Serene",
-                LastName: "Danielle"
-            }]
+
+
+        $http.get("Patients")
+            .then(function (response) {
+                $scope.Patients = response.data;
+            });
+
+ 
+        //$scope.Patients = [
+        //    {
+        //        FirstName: "John",
+        //        LastName: "Conrad"
+        //    },
+        //    {
+        //        FirstName: "Serene",
+        //        LastName: "Danielle"
+        //    }]
+
+
+        $scope.search = function (searchTerm) {
+              
+
+
+            $http.get("Patient/Search/" + searchTerm)
+                .then(function (response) {
+                    $scope.Patients = response.data; 
+                });
+
+        };
+
+        $scope.search = function (searchTerm) {
+
+
+
+            $http.get("Patient/Search/" + searchTerm)
+                .then(function (response) {
+                    $scope.Patients = response.data;
+                });
+
+        };
+
+
+        $scope.resetSearch = function () {
+
+
+
+            $http.get("Patient/Search/undefined")
+                .then(function (response) {
+                    $scope.Patients = response.data;
+                });
+
+        };
     };
+
     app.controller("PatientController", PatientController);
 }());
